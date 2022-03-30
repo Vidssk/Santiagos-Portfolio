3df83f9bf0c38bc50ea7a9fbd83a25da7d244af4
Vidssk <santy.hernandez.01@hotmail.com>
2022-03-29 20:04:01 -0500

    NewPhoto

--- src/Components/pages/Slider/SliderData.js
+++ src/Components/pages/Slider/SliderData.js
@@ -1,11 +1,14 @@
 import ZeroLatencyPhoto from './images/ZeroLatency.png';
 import JujutsuCosplay from './images/Cosplay.jpg';
+import ChicagoPhoto from './images/ChicagoPhoto.png'
+//import SnowPhoto from './images/SnowPhoto.jpg';
+//import BeanPhoto from './images/BeanPhoto.png';
+
 export const SliderData = [
-    {
-        image: ZeroLatencyPhoto
-    },
-    {
-        image: JujutsuCosplay
-    },
+    { image: ChicagoPhoto},
+    { image: ZeroLatencyPhoto },
+    { image: JujutsuCosplay },
+    //{ image: BeanPhoto },
+   // { image: SnowPhoto },
 
 ]
